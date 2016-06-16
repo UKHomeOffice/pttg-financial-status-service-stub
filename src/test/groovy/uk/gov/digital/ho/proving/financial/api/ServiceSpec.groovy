06f1deaedad9df6be84bed1e885d3a0bfe7275a8
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-06-16 09:19:25 +0100

    converted junit tests to spock/groovy(ish)

---
+++ src/test/groovy/uk/gov/digital/ho/proving/financial/api/ServiceSpec.groovy
@@ -0,0 +1,153 @@
+package uk.gov.digital.ho.proving.financial.api
+
+import com.mongodb.MongoException
+import groovy.json.JsonSlurper
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+import uk.gov.digital.ho.proving.financial.ServiceConfiguration
+import uk.gov.digital.ho.proving.financial.domain.BalanceRecord
+import uk.gov.digital.ho.proving.financial.domain.BalanceSummary
+import uk.gov.digital.ho.proving.financial.exception.AccountNotFoundException
+
+import java.time.LocalDate
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
+
+class ServiceSpec extends Specification {
+
+
+    public static final String MIN_BALANCE = "2000"
+    DataService dataMock = Mock(DataService)
+
+    Service service = new Service()
+    MockMvc mockMvc = standaloneSetup(service).build()
+
+    final String PATH = "/financialstatus/v1"
+
+    final String PATH_ACCOUNT = "/financialstatus/v1/{sortcode}/{account}/balances"
+
+    static final String SORT_CODE = "123456"
+    static final String ACCOUNT_NUMBER = "12345678"
+    static final LocalDate TR_DATE = LocalDate.parse("2014-03-25")
+
+    def setup() {
+        service.dataService = dataMock
+    }
+
+
+    def "valid account number and sortcode is queried"() {
+        given:
+        1 * dataMock.getStatement(_, _, _ , _) >> createBalanceSummary()
+
+        when:
+        def response = mockMvc.perform(get(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).param("fromDate", "2015-10-10").param("toDate", "2016-05-10"))
+
+        then:
+        def jsonContent = new JsonSlurper().parseText(response.andReturn().response.getContentAsString())
+        response.andExpect(status().isOk())
+        jsonContent.balanceRecords[0].balance == "2000"
+    }
+
+    def "bank balance test data is persisted"() {
+        given:
+        1 * dataMock.saveTestData(createBalanceSummaryNoNameDetails())
+
+        when:
+        def json = ServiceConfiguration.newInstance().mapper.writeValueAsString(createBalanceSummaryNoNameDetails().getBalanceRecords())
+        def response = mockMvc.perform(post(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+
+
+        then:
+        response.andExpect(status().isOk())
+    }
+
+    def "bank summary test data is persisted  (includes account data in request body)"() {
+        given:
+        1 * dataMock.saveTestData(createBalanceSummary())
+
+        when:
+        def json = ServiceConfiguration.newInstance().mapper.writeValueAsString(createBalanceSummary())
+        def response = mockMvc.perform(post(PATH).contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+
+        then:
+        response.andExpect(status().isOk())
+    }
+
+    def "non-existant account is queried - not found"() {
+        given:
+        1 * dataMock.getStatement(_, _, _, _) >> { throw new AccountNotFoundException() }
+
+        when:
+        def response = mockMvc.perform(get(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).param("fromDate", "2015-10-10").param("toDate", "2016-05-10"))
+
+        then:
+        response.andExpect(status().isNotFound())
+    }
+
+    def "invalid to date is rejected"() {
+        when:
+        def response = mockMvc.perform(get(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).param("fromDate", "2015-10-10").param("toDate", "2016-05-1000"))
+
+        then:
+        def jsonContent = new JsonSlurper().parseText(response.andReturn().response.getContentAsString())
+        response.andExpect(status().isBadRequest())
+        jsonContent.status.message == "Parameter error: To date is invalid"
+    }
+
+    def "invalid from date is rejected"() {
+        when:
+        def response = mockMvc.perform(get(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).param("fromDate", "20155-10-10").param("toDate", "2016-05-10"))
+
+        then:
+        def jsonContent = new JsonSlurper().parseText(response.andReturn().response.getContentAsString())
+        response.andExpect(status().isBadRequest())
+        jsonContent.status.message == "Parameter error: From date is invalid"
+    }
+
+    def "error writing to database"() {
+        given:
+        1 * dataMock.saveTestData(createBalanceSummaryNoNameDetails()) >> { throw new MongoException("a message") }
+
+        when:
+        def json = ServiceConfiguration.newInstance().mapper.writeValueAsString(createBalanceSummaryNoNameDetails().getBalanceRecords())
+        def response = mockMvc.perform(post(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+
+        then:
+        response.andExpect(status().isInternalServerError())
+    }
+
+    def "reject invalid test data - bad request"() {
+        given:
+        0 * dataMock.saveTestData(createBalanceSummaryNoNameDetails())
+
+        when:
+        def invalidjson = "some rubbish at the top" + ServiceConfiguration.newInstance().mapper.writeValueAsString(createBalanceSummaryNoNameDetails().getBalanceRecords())
+
+        def response = mockMvc.perform(post(PATH_ACCOUNT, SORT_CODE, ACCOUNT_NUMBER).contentType(MediaType.APPLICATION_JSON)
+                .content(invalidjson))
+
+        then:
+        response.andExpect(status().isBadRequest())
+    }
+
+
+    private static BalanceSummary createBalanceSummary() {
+        BalanceSummary balanceSummary = new BalanceSummary("Jane", "Brown", SORT_CODE, ACCOUNT_NUMBER)
+        balanceSummary.setBalanceRecords([new BalanceRecord(TR_DATE, MIN_BALANCE)])
+        balanceSummary
+    }
+
+    private static BalanceSummary createBalanceSummaryNoNameDetails() {
+        BalanceSummary balanceSummary = new BalanceSummary(SORT_CODE, ACCOUNT_NUMBER)
+        balanceSummary.setBalanceRecords([new BalanceRecord(TR_DATE, MIN_BALANCE)])
+        balanceSummary
+    }
+
+}
---
+++ src/test/groovy/uk/gov/digital/ho/proving/financial/domain/BalanceSummaryJsonSpec.groovy
@@ -0,0 +1,57 @@
+package uk.gov.digital.ho.proving.financial.domain
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import spock.lang.Specification
+import uk.gov.digital.ho.proving.financial.ServiceConfiguration
+
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDate
+
+import static org.codehaus.groovy.runtime.StringGroovyMethods.replaceAll
+
+class BalanceSummaryJsonSpec extends Specification{
+    ObjectMapper mapper = new ServiceConfiguration().getMapper()
+
+
+    def "Balance Record should serialize to expected json format"() {
+
+        given:
+        def balanceSummary = getBalanceSummary()
+
+        when: "balance record is serialized"
+        def jsonInString = mapper.writeValueAsString(balanceSummary)
+
+        then: "the output matches the expected json (once whitespace removed)"
+        jsonInString.replaceAll("\\s","") ==  getJsonResource("balanceSummaryJsonTest.json").replaceAll("\\s","")
+    }
+
+    def "Balance Record in json format should deserialize to expected object"() {
+
+        given:
+        def jsonString = getJsonResource("balanceSummaryJsonTest.json")
+
+        when: "json string is deserialized"
+        BalanceSummary balanceSummary = mapper.readValue(jsonString, BalanceSummary.class)
+
+        then:
+        balanceSummary == getBalanceSummary()
+    }
+
+    def getJsonResource(String file){
+        try {
+            URI uri = this.getClass().getClassLoader().getResource(file).toURI()
+            new String(Files.readAllBytes(Paths.get(uri)), Charset.forName("utf-8"))
+        } catch (URISyntaxException | IOException e) {
+            println("Problem reading resource file",e)
+        }
+    }
+
+    def BalanceSummary getBalanceSummary() {
+        BalanceSummary st = new BalanceSummary("Ray", "Purchase", "601234", "12345678")
+        st.setBalanceRecords([new BalanceRecord(LocalDate.parse("2016-10-08"), "3000"),new BalanceRecord(LocalDate.parse("2016-10-07"), "3000")])
+        st
+    }
+
+}
