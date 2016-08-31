a3877c261f171e228ca907b34b9568945c55c791
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-08-31 14:19:07 +0100

    removing dsp specific property files after switch to ENV variables

--- src/main/java/uk/gov/digital/ho/proving/financial/ServiceConfiguration.java
+++ src/main/java/uk/gov/digital/ho/proving/financial/ServiceConfiguration.java
@@ -4,31 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
-@PropertySource(value = "classpath:dsp-default.properties")
-@PropertySource(value = "classpath:/developer/developer-default.properties", ignoreResourceNotFound = true)
-@PropertySource(value = "classpath:/developer/${user.name}-default.properties", ignoreResourceNotFound = true)
 public class ServiceConfiguration {
 
     private static Logger LOGGER = LoggerFactory.getLogger(ServiceConfiguration.class);
