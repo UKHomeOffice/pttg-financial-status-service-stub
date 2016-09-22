9608e7081c0d90342c1b75b59909469fd3f43c42
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-09-22 13:10:45 +0100

    configuring healthcheck

--- src/main/java/uk/gov/digital/ho/proving/financial/health/MongoTestDataHealthIndicator.java
+++ src/main/java/uk/gov/digital/ho/proving/financial/health/MongoTestDataHealthIndicator.java
@@ -1,22 +1,17 @@
 package uk.gov.digital.ho.proving.financial.health;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 import uk.gov.digital.ho.proving.financial.dao.BalanceSummaryRepository;
-import uk.gov.digital.ho.proving.financial.domain.BalanceSummary;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.List;
 
 /**
  * @Author Home Office Digital
+ * There is a default springboot healthcheck for the mongotemplate - this healthcheck indicates if the mongo instance is returning test data
  */
 @Component
-public class MongoConnectionHealth implements HealthIndicator {
+public class MongoTestDataHealthIndicator implements HealthIndicator {
 
     @Autowired
     private BalanceSummaryRepository repository;
@@ -29,16 +24,16 @@ public class MongoConnectionHealth implements HealthIndicator {
         try {
             final long count = repository.count();
             if (count == 0) {
-                return Health.up().withDetail("Mongo is available but not populated with test data", "UP").build();
+                return Health.down().withDetail("Mongo is available but not populated with test data", "DOWN").build();
             }
 
             if (count > 0) {
-                return Health.up().withDetail("Mongo is available and populated with test data: ", "UP").build();
+                return Health.up().withDetail("Mongo is available and populated with test data", "UP").build();
             }
         } catch (Exception e) {
             message = e.getMessage();
         }
-        return Health.down().withDetail("Mongo was not available: " + message, "Down").build();
+        return Health.down().withDetail("Mongo was not available: " + message, "DOWN").build();
     }
 
 }
--- src/test/groovy/uk/gov/digital/ho/proving/financial/health/MongoTestDataHealthIndicatorSpec.groovy
+++ src/test/groovy/uk/gov/digital/ho/proving/financial/health/MongoTestDataHealthIndicatorSpec.groovy
@@ -6,55 +6,60 @@ import spock.lang.Specification
 import uk.gov.digital.ho.proving.financial.dao.BalanceSummaryRepository;
 
 
-class MongoConnectionHealthTest extends Specification {
+class MongoTestDataHealthIndicatorSpec extends Specification {
 
-    /*def "should report DOWN when mongo unreachable"() {
+    def "should report DOWN when mongo unreachable"() {
 
         given:
-        MongoConnectionHealth healthCheck = new MongoConnectionHealth()
-        healthCheck.apiRoot = ''
-        healthCheck.apiEndpoint = ''
+        MongoTestDataHealthIndicator healthCheck = new MongoTestDataHealthIndicator()
+        def repository = Mock(BalanceSummaryRepository)
+        healthCheck.repository = repository
+
+        and:
+        repository.count() >> {throw new RuntimeException ("Something went wrong when accessing mongo")}
+
+        when:
+        Health result = healthCheck.health()
+
+        then:
+        notThrown(Exception)
+        result.getStatus() == Status.DOWN
+    }
+
+    def "should report UP when mongo is reachable and is populated with data"() {
+
+        given:
+        MongoTestDataHealthIndicator healthCheck = new MongoTestDataHealthIndicator()
+        def repository = Mock(BalanceSummaryRepository)
+        healthCheck.repository = repository
+
+        and:
+        repository.count() >> 200
+
+        when:
+        Health result = healthCheck.health()
+
+        then:
+        repository.count() * 1
+        result.getStatus() == Status.UP
+        result.getDetails().containsKey("Mongo is available and populated with test data")
+    }
+
+    def "should report DOWN when mongo is reachable but empty"() {
+
+        given:
+        MongoTestDataHealthIndicator healthCheck = new MongoTestDataHealthIndicator()
+        def repository = Mock(BalanceSummaryRepository)
+        healthCheck.repository = repository
+
+        and:
+        repository.count() >> 0
 
         when:
         Health result = healthCheck.health()
 
         then:
         result.getStatus() == Status.DOWN
-    }*/
-
-    def "should report UP when mongo is reachable and is populated with data"() {
-
-        given:
-        MongoConnectionHealth healthCheck = new MongoConnectionHealth()
-        def repository = Mock(BalanceSummaryRepository)
-        healthCheck.repository = repository
-
-        and:
-        repository.count >> 200
-
-        when:
-        Health result = healthCheck.health()
-
-        then:
-        result.getStatus() == Status.UP
-        result.getDetails().containsKey("Mongo is available but not populated with test data")
-    }
-
-    def "should report UP when mongo is reachable but empty"() {
-
-        given:
-        MongoConnectionHealth healthCheck = new MongoConnectionHealth()
-        def repository = Mock(BalanceSummaryRepository)
-        healthCheck.repository = repository
-
-        and:
-        repository.count >> 0
-
-        when:
-        Health result = healthCheck.health()
-
-        then:
-        result.getStatus() == Status.UP
         result.getDetails().containsKey("Mongo is available but not populated with test data")
     }
 }
